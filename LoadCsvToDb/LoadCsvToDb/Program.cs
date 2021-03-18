696024673a901ea926b0991499b36c6bc60243c5
jabyz <javiernba@gmail.com>
2021-03-17 20:22:52 -0600

    Initial Commit

---
+++ LoadCsvToDb/LoadCsvToDb/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LoadCsvToDb
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        //PROVIDE LOCAL FOLDER AND NAME OF THE CSV
+        string csvFile = Path.Combine(@"‪C:\myDir\", "myCSV.csv");
+        //CONFIGURE CONNECTION STRING TO CSV FILE
+        string extendedProperties = @"""text;HDR=YES;FMT=Delimited;Format=Delimited(,)"";";
+        string csvConn = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\myDir\;Extended Properties=" + extendedProperties + "";
+        OleDbConnection oleDbConn = new OleDbConnection(csvConn);
+        oleDbConn.Open();
+        OleDbCommand oleDbCmd = new OleDbCommand("SELECT * FROM [" + Path.GetFileName(csvFile) + "]", oleDbConn);
+        OleDbDataReader oleDbRdr = oleDbCmd.ExecuteReader();
+        int colCount = oleDbRdr.FieldCount;
+        oleDbRdr.Close();
+        DataTable dt = new DataTable();
+        OleDbDataAdapter da = new OleDbDataAdapter("SELECT * FROM [" + Path.GetFileName(csvFile) + "]", oleDbConn);
+        da.Fill(dt);
+        //PROVIDE CONNECTION STRING TO DATABASE
+        SqlBulkCopy sqlBulkCopy = new SqlBulkCopy("Data Source=.;Initial Catalog=xCart; Integrated Security=true");
+        //SPECIFY TABLE IN DATABASE TO LOAD THE DATA INTO
+        sqlBulkCopy.DestinationTableName = "[orders]";
+        sqlBulkCopy.BatchSize = 0;
+        sqlBulkCopy.BulkCopyTimeout = 0;
+        sqlBulkCopy.WriteToServer(dt);
+        sqlBulkCopy.Close();
+      }
+      catch (Exception ex)
+      {
+
+        Console.WriteLine(ex.Message.ToString());
+      }
+      
+    }
+  }
+}
---
+++ LoadCsvToDb/LoadCsvToDb/sqlService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LoadFilesToDb
+{
+  class sqlService
+  {
+    public SqlConnection getSqlConn()
+    {
+      string conn = System.Configuration.ConfigurationManager.ConnectionStrings["NOVA-E10-TSQL"].ConnectionString;
+      SqlConnection sqlConn = new SqlConnection(conn);
+
+      return sqlConn;
+    }
+
+    public DataTable cmdToDataTable(SqlCommand cmd)
+    {
+      using (SqlConnection sqlConn = getSqlConn())
+      {
+        if (sqlConn.State == ConnectionState.Open)
+          sqlConn.Close();
+        try
+        {
+          cmd.Connection = sqlConn;
+          cmd.CommandTimeout = 0;
+          sqlConn.Open();
+          SqlDataAdapter da = new SqlDataAdapter(cmd);
+          DataTable dt = new DataTable();
+          da.Fill(dt);
+          cmd.Dispose();
+          sqlConn.Close();
+          sqlConn.Dispose();
+          return dt;
+        }
+        catch (Exception ex)
+        {
+
+          cmd.Dispose();
+          sqlConn.Close();
+          sqlConn.Dispose();
+          return null;
+        }
+      }
+    }
+
+    public int xQuery(SqlCommand cmd)
+    {
+      int ra = 0;
+      using (SqlConnection sqlConn = getSqlConn())
+      {
+        if (sqlConn.State == ConnectionState.Open)
+          sqlConn.Close();
+        try
+        {
+          cmd.Connection = sqlConn;
+          cmd.CommandTimeout = 0;
+          sqlConn.Open();
+          ra = cmd.ExecuteNonQuery();
+
+          cmd.Dispose();
+          sqlConn.Close();
+          sqlConn.Dispose();
+
+          return ra;
+        }
+        catch (Exception ex)
+        {
+
+          cmd.Dispose();
+          sqlConn.Close();
+          sqlConn.Dispose();
+          return 0;
+        }
+      }
+    }
+  }
+}
